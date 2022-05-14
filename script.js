2d0279cdf65e8feee1e618b6a55c1e88d05622a1
Badra Mohamed <moha1150@umn.edu>
2022-05-14 16:31:53 -0500

    second

--- script.js
+++ script.js
@@ -11,5 +11,25 @@ function onReady( ) {
    console.log('title header is', titleHeader)
    // console.log('titleHeader', <h1>....</h1>)
 
-   $(document).on(click, #submit-btn, onsubmit);
+   $(document).on('click', '#submit-btn', onsubmit);
+
+
 }
+function onsubmit() {
+    let firstName=$('.first-name').val();
+    console.log('firstName', firstName);
+    
+    let lastName= $('.last-name').val();
+    console.log('last-name', lastName);
+    
+   let idNumber= $('.id-number').val();
+    console.log('id-number', idNumber);
+
+    let Title= $('.title-name').val();
+    console.log('title-name', Title);
+    
+    let annualSalary= $('.annual-salary').val();
+    console.log('annual-salary', annualSalary);
+
+}
+
